1be5021a846386a19060901bf5c66d66ecd600a0
irfanfajru <irfanfajru025@gmail.com>
2023-04-02 02:23:34 +0700

    post and get image created

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-from flask import Flask, request
+from flask import Flask, request, send_file, url_for
 from markupsafe import escape
+from werkzeug.utils import secure_filename
+import uuid
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<p>Hello world</p>"
+    return "<p>Hello Dek, mau ngapain ?</p>"
 
-
-# route with parameter
-
-
-@app.route("/<name>")
-def name(name):
-    return f"<p>hello {escape(name)}</p>"
+# post detection image
 
 
 @app.post("/detect")
 def detect():
-    return "ini hasil deteksi anda"
+    file = request.files['image']
+    filename = f"{str(uuid.uuid4())}{secure_filename(file.filename)}"
+    file.save(f"storage/{filename}")
+    return {
+        "success": True,
+        "message": "Gambar berhasil dideteksi",
+        "data": {
+            "resultUrl": url_for("result", filename=filename, _external=True),
+        }
+    }
+
+# get result
+
+
+@ app.get("/result/<filename>")
+def result(filename):
+    return send_file(f"storage/{secure_filename(filename)}")
