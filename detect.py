0ea0ba9efb309d099f60f476057309c274e4a3d8
irfanfajru <irfanfajru025@gmail.com>
2023-04-02 03:08:17 +0700

    add utils and models

--- detect.py
+++ detect.py
@@ -15,22 +15,13 @@ from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 
 
-def detect(filePath, weights="yolov7.pt", imageSize=640, save_img=True):
-    source, weights, view_img, save_txt, imgsz, trace = filePath, weights, opt.view_img, opt.save_txt, imageSize, not opt.no_trace
-    save_img = not opt.nosave and not source.endswith(
-        '.txt')  # save inference images
-    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-        ('rtsp://', 'rtmp://', 'http://', 'https://'))
-
+def detect(filePath, classes=2, threshold=0.6, saveDir="storage", weights="yolov7.pt", imageSize=640, save_img=True):
+    source, weights, view_img, save_txt, imgsz, trace = filePath, weights, False, False, imageSize, not False
     # Directories
-    save_dir = Path(increment_path(Path(opt.project) / opt.name,
-                    exist_ok=opt.exist_ok))  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True,
-                                                          exist_ok=True)  # make dir
-
+    save_dir = Path(saveDir)
     # Initialize
     set_logging()
-    device = select_device(opt.device)
+    device = select_device(1)
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
@@ -39,7 +30,7 @@ def detect(filePath, weights="yolov7.pt", imageSize=640, save_img=True):
     imgsz = check_img_size(imgsz, s=stride)  # check img_size
 
     if trace:
-        model = TracedModel(model, device, opt.img_size)
+        model = TracedModel(model, device, imageSize)
 
     if half:
         model.half()  # to FP16
@@ -52,13 +43,7 @@ def detect(filePath, weights="yolov7.pt", imageSize=640, save_img=True):
             'weights/resnet101.pt', map_location=device)['model']).to(device).eval()
 
     # Set Dataloader
-    vid_path, vid_writer = None, None
-    if webcam:
-        view_img = check_imshow()
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
-    else:
-        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+    dataset = LoadImages(source, img_size=imgsz, stride=stride)
 
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
@@ -85,17 +70,17 @@ def detect(filePath, weights="yolov7.pt", imageSize=640, save_img=True):
             old_img_h = img.shape[2]
             old_img_w = img.shape[3]
             for i in range(3):
-                model(img, augment=opt.augment)[0]
+                model(img, augment=False)[0]
 
         # Inference
         t1 = time_synchronized()
         with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
-            pred = model(img, augment=opt.augment)[0]
+            pred = model(img, augment=False)[0]
         t2 = time_synchronized()
 
         # Apply NMS
         pred = non_max_suppression(
-            pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+            pred, threshold, threshold, classes=classes, agnostic=False)
         t3 = time_synchronized()
 
         # Apply Classifier
@@ -104,11 +89,7 @@ def detect(filePath, weights="yolov7.pt", imageSize=640, save_img=True):
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
-            if webcam:  # batch_size >= 1
-                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(
-                ), dataset.count
-            else:
-                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+            p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
 
             p = Path(p)  # to Path
             save_path = str(save_dir / p.name)  # img.jpg
@@ -134,7 +115,7 @@ def detect(filePath, weights="yolov7.pt", imageSize=640, save_img=True):
                                           ) / gn).view(-1).tolist()  # normalized xywh
                         # label format
                         line = (
-                            cls, *xywh, conf) if opt.save_conf else (cls, *xywh)
+                            cls, *xywh, conf) if threshold else (cls, *xywh)
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
@@ -146,11 +127,6 @@ def detect(filePath, weights="yolov7.pt", imageSize=640, save_img=True):
             # Print time (inference + NMS)
             print(
                 f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
-
-            # Stream results
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
 
             # Save results (image with detections)
             if save_img:
@@ -179,55 +155,3 @@ def detect(filePath, weights="yolov7.pt", imageSize=640, save_img=True):
         # print(f"Results saved to {save_dir}{s}")
 
     print(f'Done. ({time.time() - t0:.3f}s)')
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str,
-                        default='yolov7.pt', help='model.pt path(s)')
-    # file/folder, 0 for webcam
-    parser.add_argument('--source', type=str,
-                        default='inference/images', help='source')
-    parser.add_argument('--img-size', type=int, default=640,
-                        help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float,
-                        default=0.25, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float,
-                        default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='',
-                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true',
-                        help='display results')
-    parser.add_argument('--save-txt', action='store_true',
-                        help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true',
-                        help='save confidences in --save-txt labels')
-    parser.add_argument('--nosave', action='store_true',
-                        help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int,
-                        help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true',
-                        help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true',
-                        help='augmented inference')
-    parser.add_argument('--update', action='store_true',
-                        help='update all models')
-    parser.add_argument('--project', default='runs/detect',
-                        help='save results to project/name')
-    parser.add_argument('--name', default='exp',
-                        help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true',
-                        help='existing project/name ok, do not increment')
-    parser.add_argument('--no-trace', action='store_true',
-                        help='don`t trace model')
-    opt = parser.parse_args()
-    print(opt)
-    # check_requirements(exclude=('pycocotools', 'thop'))
-
-    with torch.no_grad():
-        if opt.update:  # update all models (to fix SourceChangeWarning)
-            for opt.weights in ['yolov7.pt']:
-                detect()
-                strip_optimizer(opt.weights)
-        else:
-            detect()
